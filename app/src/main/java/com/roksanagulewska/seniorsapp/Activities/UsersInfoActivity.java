95039fbe4a7ba858044535f2715136951dc8c2af
Roksana <rgulewska@gmail.com>
2021-11-28 17:38:08 +0100

    poprawiona walidacja w usersInfoActivity

--- app/src/main/java/com/roksanagulewska/seniorsapp/Activities/UsersInfoActivity.java
+++ app/src/main/java/com/roksanagulewska/seniorsapp/Activities/UsersInfoActivity.java
@@ -2,13 +2,16 @@ package com.roksanagulewska.seniorsapp.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+
 import com.roksanagulewska.seniorsapp.R;
 
 public class UsersInfoActivity extends AppCompatActivity {
@@ -18,8 +21,8 @@ public class UsersInfoActivity extends AppCompatActivity {
     EditText ageEditTxt;
     SwitchCompat sexSwitch;
     Button confirmInfoBtn;
-    String sex = "female";
-    int age = 0;
+    String sex = "female"; //domyślna płeć to kobieta, ponieważ taką wartość reprezentuje switch w swoim położeniu startowym
+    int age; // ustawiam wartość początkową wieku na 0, aby upewnić się że użytkownik wprowadził jakąś wartość
 
 
     @Override
@@ -31,21 +34,21 @@ public class UsersInfoActivity extends AppCompatActivity {
         localisationEditTxt = findViewById(R.id.localisationEditText);
         ageEditTxt = findViewById(R.id.ageEditText);
         sexSwitch = findViewById(R.id.sexSwitch);
-        confirmInfoBtn = findViewById(R.id.confirmInfoButton);
+        confirmInfoBtn = findViewById(R.id.confirmInfoButton); //przycisk potwierdzający wpisane informacje
 
         Intent registerIntent = getIntent(); //intencja która wywołała tą aktywność
         Bundle registerBundle = registerIntent.getExtras(); // przypisanie bundla który przyszedł z intencją
-        String email = registerBundle.getString("email");
-        String password = registerBundle.getString("password");
+        String email = registerBundle.getString("email"); //email przysłany z aktywności RegistrationActivity
+        String password = registerBundle.getString("password"); //hasło przysłane z aktywności RegistrationActivity
 
         //sprawdzanie stanu switcha
         sexSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                if(isChecked) { //Male
+                if (isChecked) { //jeżeli został przesunięty
                     sex = "male";
-                } else { //Female
+                } else { //jeżeli jest w pozycji startowej
                     sex = "female";
                 }
             }
@@ -55,31 +58,64 @@ public class UsersInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String name = nameEditTxt.getText().toString().trim();
-                String localisation = localisationEditTxt.getText().toString().trim();
-                age = Integer.parseInt(ageEditTxt.getText().toString().trim());
+                String name = nameEditTxt.getText().toString().trim(); //przypisywanie zmiennej name, tekstu wpisanego w pole nameEditText, trim() pozbywa się białych znaków z początku i końca łańcucha
+                String localisation = localisationEditTxt.getText().toString().trim(); //przypisywanie zmiennej localisation, tekstu wpisanego w pole nameEditText, trim() pozbywa się białych znaków z początku i końca łańcucha
 
-                //wywala wyjątek jak localisation albo name puste
-                name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase(); //formatowanie imienia tak aby zaczynało się od wielkiej litery
-                localisation = localisation.substring(0,1).toUpperCase() + localisation.substring(1).toLowerCase(); //formatowanie miasta tak aby zaczynało się od wielkiej litery
 
-                if(name.isEmpty() || localisation.isEmpty() || age == 0)
-                {
-                    Toast.makeText(getApplicationContext(), "Please add all required information.", Toast.LENGTH_SHORT).show();
+                try {
+                    age = Integer.parseInt(ageEditTxt.getText().toString().trim()); //przypisanie zmiennej age wartości wpisanej w pole ageEditText
+                } catch (NumberFormatException exception) { //jeżeli wpisany text nie będzie liczbą, zostanie wyrzucony wyjątek
+                    if (age != 0) {
+                        Toast.makeText(getApplicationContext(), "Incorrect age format.", Toast.LENGTH_LONG).show(); //komunikat informujący o wyrzuceniu wyjątku
+                    }
+                }
+
+
+                if (name.isEmpty() || localisation.isEmpty() || ageEditTxt.getText().toString().isEmpty()) { //sprawdzenie czy wszystkie pola zostały uzupełnione
+                    Toast.makeText(getApplicationContext(), "Please add all required information.", Toast.LENGTH_SHORT).show(); //jeśli nie, wyświetlany jest komunikat
                 } else {
-                    Intent infoIntent = new Intent(getApplicationContext(), PreferencesActivity.class);
-                    Bundle infoBundle = new Bundle();
-                    infoBundle.putString("email", email);
-                    infoBundle.putString("password", password);
-                    infoBundle.putString("name", name);
-                    infoBundle.putString("localisation", localisation);
-                    infoBundle.putInt("age", age);
-                    infoBundle.putString("sex", sex);
-                    infoIntent.putExtras(infoBundle);
-                    startActivity(infoIntent);
-                    finish();
+                    if (containsLettersOnly(name) && containsLettersOnly(localisation)) { //sprawdzenie czy do pól localisation zostały wprowadzone łańcuchy znaków zawierające tylko litery
+                        if (age >= 130 || age < 18) { //sprawdzenie czy podany wiek jest ralistyczny i zgodny z przeznaczeniem aplikacji
+                            Toast.makeText(getApplicationContext(), "Insert correct age, it must be greater or equal to 18.", Toast.LENGTH_LONG).show();
+                        } else { //jeżeli wszystkie powyższe warunki mają odpowiednie wartości
+
+                            name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase(); //formatowanie imienia tak aby zaczynało się od wielkiej litery
+                            localisation = localisation.substring(0, 1).toUpperCase() + localisation.substring(1).toLowerCase(); //formatowanie miasta tak aby zaczynało się od wielkiej litery
+
+                            Intent infoIntent = new Intent(getApplicationContext(), PreferencesActivity.class);
+                            Bundle infoBundle = new Bundle();
+                            infoBundle.putString("email", email);
+                            infoBundle.putString("password", password);
+                            infoBundle.putString("name", name);
+                            infoBundle.putString("localisation", localisation);
+                            infoBundle.putInt("age", age);
+                            infoBundle.putString("sex", sex);
+                            infoIntent.putExtras(infoBundle); //przyłączanie bundla z danymi wprowadzonymi przez użytkownika do intencji
+                            startActivity(infoIntent);
+                            finish();
+
+                        }
+                    } else { //jeśli localisation i name składają się z innych znaków niż litery
+                        Toast.makeText(getApplicationContext(), "Name and localisation must contain only letters.", Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
+    }
+
+    //metoda sprawdzająca czy łańcuch podany w argumencie zawiera tylko litery
+    public boolean containsLettersOnly(String text) {
+        char[] textCharacters = text.toCharArray();
+        boolean isIt = true;
+
+        for (char character : textCharacters) {
+            if (!Character.isLetter(character)) {
+                Log.d("VALID", character + " is NOT letter");
+                isIt = false;
+                return isIt;
+            }
+            Log.d("VALID", character + " is letter");
+        }
+        return isIt;
     }
 }
